2eb66771f5e094297a770609406d2b7045f0b897
DiSonDS <dison.ds@gmail.com>
2023-01-23 14:29:58 +0300

    feat: replace RealTimeSignalProcessor with BaseSignalProcessor

--- django_elasticsearch_dsl_celery/__init__.py
+++ django_elasticsearch_dsl_celery/__init__.py
@@ -1,12 +1,12 @@
 from celery import shared_task
 from django.apps import apps
-from django.db import transaction
+from django.db import transaction, models
 from django_elasticsearch_dsl.registries import registry
-from django_elasticsearch_dsl.signals import RealTimeSignalProcessor
+from django_elasticsearch_dsl.signals import BaseSignalProcessor
 
 
 @shared_task
-def handle_save(pk,  app_label, model_name):
+def handle_save(pk, app_label, model_name):
     sender = apps.get_model(app_label, model_name)
     instance = sender.objects.get(pk=pk)
     registry.update(instance)
@@ -14,20 +14,20 @@ def handle_save(pk,  app_label, model_name):
 
 
 @shared_task
-def handle_pre_delete(pk,  app_label, model_name):
+def handle_pre_delete(pk, app_label, model_name):
     sender = apps.get_model(app_label, model_name)
     fake_instance = sender(pk=pk)
     registry.delete_related(fake_instance)
 
 
 @shared_task
-def handle_delete(pk,  app_label, model_name):
+def handle_delete(pk, app_label, model_name):
     sender = apps.get_model(app_label, model_name)
     fake_instance = sender(pk=pk)
     registry.delete(fake_instance, raise_on_error=False)
 
 
-class CelerySignalProcessor(RealTimeSignalProcessor):
+class CelerySignalProcessor(BaseSignalProcessor):
     """Celery signal processor.
     Allows automatic updates on the index as delayed background tasks using
     Celery.
@@ -38,6 +38,22 @@ class CelerySignalProcessor(RealTimeSignalProcessor):
     but using `pickle` opens the application up to security concerns.
     """
 
+    def setup(self):
+        # Listen to all model saves.
+        models.signals.post_save.connect(self.handle_save)
+        models.signals.post_delete.connect(self.handle_delete)
+
+        # Use to manage related objects update
+        models.signals.m2m_changed.connect(self.handle_m2m_changed)
+        models.signals.pre_delete.connect(self.handle_pre_delete)
+
+    def teardown(self):
+        # Listen to all model saves.
+        models.signals.post_save.disconnect(self.handle_save)
+        models.signals.post_delete.disconnect(self.handle_delete)
+        models.signals.m2m_changed.disconnect(self.handle_m2m_changed)
+        models.signals.pre_delete.disconnect(self.handle_pre_delete)
+
     def handle_save(self, sender, instance, **kwargs):
         """Handle save.
 
@@ -46,7 +62,9 @@ class CelerySignalProcessor(RealTimeSignalProcessor):
         """
         app_label = instance._meta.app_label
         model_name = instance._meta.model_name
-        transaction.on_commit(lambda: handle_save.delay(instance.pk, app_label, model_name))
+        transaction.on_commit(
+            lambda: handle_save.delay(instance.pk, app_label, model_name)
+        )
 
     def handle_pre_delete(self, sender, instance, **kwargs):
         """Handle removing of instance object from related models instance.
